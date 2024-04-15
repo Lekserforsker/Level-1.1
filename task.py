1e462ca641108ffa0e7191e0f92a0d8b43c81462
Lekserforsker <157828875+Lekserforsker@users.noreply.github.com>
2024-04-15 19:56:00 +0300

    Update task.py

--- task.py
+++ task.py
@@ -1,13 +1,27 @@
 def print_staircase(size):
     for i in range(1, size + 1):
-        print(" " * (size - i) + "*" * i)
 
-def print_double_sided_staircase(size):
+        print(' ' * (size - i), end='')
+        # Print staircase
+        print('*' * i)
+
+def print_double_staircase(size):
     for i in range(1, size + 1):
-        print(" " * (size - i) + "*" * i + " " * (size - i) + "*" * i)
+        # Print leading spaces
+        print(' ' * (size - i), end='')
+        # Print left side of the staircase
+        print('*' * i, end=' ')
+        # Print right side of the staircase
+        print('*' * i)
+
 
 size = int(input("Enter the size of the staircase: "))
+
+
 print("Single-sided staircase:")
 print_staircase(size)
+
+
 print("\nDouble-sided staircase:")
-print_double_sided_staircase(size)
+print_double_staircase(size)
+
